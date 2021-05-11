020d55f946006da7fc563f00f2bc4b423e4275a3
Ca-moes <andre3000pt@gmail.com>
2021-05-11 09:27:52 +0100

    Board and starting to check available moves

--- examples/demo.py
+++ examples/demo.py
@@ -1,2 +1,14 @@
 # Onde vai ocorrer o jogo e haver a interação Agente-Ambiente
 # https://github.com/towzeur/gym-abalone/blob/master/examples/demo.py
+import numpy
+
+from gym_neutreeko.game.engine.gamelogic import NeutreekoGame
+
+if __name__ == "__main__":
+    game = NeutreekoGame()
+    board = NeutreekoGame.new_board()
+
+    result = numpy.where(board == 1)
+    print(result)
+    listOfCoordinates = list(zip(result[0], result[1]))
+    print(listOfCoordinates)
--- gym_neutreeko/game/engine/gamelogic.py
+++ gym_neutreeko/game/engine/gamelogic.py
@@ -1,1 +1,33 @@
 # Lógica do Jogo: https://github.com/towzeur/gym-abalone/blob/master/gym_abalone/game/engine/gamelogic.py
+import numpy
+import numpy as np
+
+
+class NeutreekoGame:
+    # PLAYERS
+    BLACK = 2
+    WHITE = 1
+
+    def __init__(self):
+        self.board = None
+        self.turn = None
+
+    @staticmethod
+    def new_board():
+        """
+        returns a fresh starting board
+        :return: numpy.array
+        """
+        return np.array([[0, 1, 0, 1, 0],
+                         [0, 0, 2, 0, 0],
+                         [0, 0, 0, 0, 0],
+                         [0, 0, 1, 0, 0],
+                         [0, 2, 0, 2, 0]])
+
+    def get_possible_moves(self, player: int):
+        pass
+        # Encontra peças do player (a fazer em main)
+
+        # Vê quais as direções válidas para essa peça (função à parte para fazer)
+
+        # retorna isso
